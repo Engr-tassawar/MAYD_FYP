c1ffb82477bf6335492b72fe42508049a80811c6
Tassawar Abbas <tassawarabbas034@gmail.com>
2022-04-19 16:45:21 +0500

    change_1

--- app/src/main/java/com/example/mayd/navigationBar.java
+++ app/src/main/java/com/example/mayd/navigationBar.java
@@ -10,5 +10,7 @@ public class navigationBar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_bar);
+
+        int x=3;
     }
 }
