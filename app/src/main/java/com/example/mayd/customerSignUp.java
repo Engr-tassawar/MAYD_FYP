d64ffaa2a24756923acebc80583dc1d80d386f39
JawaddAhmed <jawad.bsse3862@iiu.edu.pk>
2022-06-03 02:32:47 +0500

    checks on java code backend

--- app/src/main/java/com/example/mayd/customerSignUp.java
+++ app/src/main/java/com/example/mayd/customerSignUp.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -30,26 +29,40 @@ public class customerSignUp extends AppCompatActivity {
         btnSignUp=findViewById(R.id.Customer_btnSignup_P2);
         tvSignIn=findViewById(R.id.tvAlreadyAccountSignUp_P2);
 
-       tvSignIn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               Intent intent=new Intent(customerSignUp.this, sign_in_ForCustomer.class);
-               startActivity(intent);
-           }
+       tvSignIn.setOnClickListener(view -> {
+           Intent intent=new Intent(customerSignUp.this, sign_in_ForCustomer.class);
+           startActivity(intent);
        });
 
-      btnSignUp.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              String FullName=edtFullName.getText().toString().trim();
-              String Email=edtEmail.getText().toString().trim();
-              String Phone=edtPhone.getText().toString().trim();
-              String Password=edtPassword.getText().toString();
-              String ConfirmPassword=edtConfirmPassword.getText().toString();
+      btnSignUp.setOnClickListener(view -> {
+          String FullName=edtFullName.getText().toString().trim();
+          String Email=edtEmail.getText().toString().trim();
+          String Phone=edtPhone.getText().toString().trim();
+          String Password=edtPassword.getText().toString();
+          String ConfirmPassword=edtConfirmPassword.getText().toString();
 
-              if (FullName.equals("")){
-                  edtFullName.setError("This Place Cannot Be Empty");
-              }
+          if (FullName.equals("")){
+              edtFullName.setError("This Place Cannot Be Empty");
+          }
+
+          if (Email.equals("")){
+              edtEmail.setError("This Place Cannot Be Empty");
+          }
+
+          if (Phone.equals("")){
+              edtPhone.setError("This Place Cannot Be Empty");
+          }
+
+          if (Password.equals("")){
+              edtPassword.setError("This Place Cannot Be Empty");
+          }
+
+          if (ConfirmPassword.equals(Password)){
+              edtConfirmPassword.setError("Password Matched Successfully");
+
+          }
+          else {
+              edtConfirmPassword.setError("The Password DidNot Match");
           }
       });
 
