2879a144f89d974d0c53af2844db352517320494
JawaddAhmed <jawad.bsse3862@iiu.edu.pk>
2022-06-05 16:18:52 +0500

    friebase connection done

--- app/src/main/java/com/example/mayd/customerSignUp.java
+++ app/src/main/java/com/example/mayd/customerSignUp.java
@@ -1,17 +1,32 @@
 package com.example.mayd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class customerSignUp extends AppCompatActivity {
-    EditText edtFullName,edtEmail,edtPhone,edtPassword,edtConfirmPassword;
+    private static final String TAG = "tag";
+    EditText edtFullName, edtEmail, edtPhone, edtPassword, edtConfirmPassword;
     Button btnSignUp;
     TextView tvSignIn;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,50 +35,84 @@ public class customerSignUp extends AppCompatActivity {
         //For hiding Action Bar
         //getSupportActionBar().hide();
 
-        edtFullName=findViewById(R.id.Customer_edtFullNameSignUp);
-        edtEmail=findViewById(R.id.Customer_edtEmailSignUp);
-        edtPhone=findViewById(R.id.Customer_edtPhoneSignUp);
-        edtPassword=findViewById(R.id.Customer_edtPasswordSignUp);
-        edtConfirmPassword=findViewById(R.id.Customer_edtConfirmPasswordSignUp);
 
-        btnSignUp=findViewById(R.id.Customer_btnSignup_P2);
-        tvSignIn=findViewById(R.id.tvAlreadyAccountSignUp_P2);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
-       tvSignIn.setOnClickListener(view -> {
-           Intent intent=new Intent(customerSignUp.this, sign_in_ForCustomer.class);
-           startActivity(intent);
-       });
+        edtFullName = findViewById(R.id.Customer_edtFullNameSignUp);
+        edtEmail = findViewById(R.id.Customer_edtEmailSignUp);
+        edtPhone = findViewById(R.id.Customer_edtPhoneSignUp);
+        edtPassword = findViewById(R.id.Customer_edtPasswordSignUp);
+        edtConfirmPassword = findViewById(R.id.Customer_edtConfirmPasswordSignUp);
 
-      btnSignUp.setOnClickListener(view -> {
-          String FullName=edtFullName.getText().toString().trim();
-          String Email=edtEmail.getText().toString().trim();
-          String Phone=edtPhone.getText().toString().trim();
-          String Password=edtPassword.getText().toString();
-          String ConfirmPassword=edtConfirmPassword.getText().toString();
+        btnSignUp = findViewById(R.id.Customer_btnSignup_P2);
+        tvSignIn = findViewById(R.id.tvAlreadyAccountSignUp_P2);
 
-          if (FullName.equals("")){
-              edtFullName.setError("This Place Cannot Be Empty");
-          }
+        tvSignIn.setOnClickListener(view -> {
+            Intent intent = new Intent(customerSignUp.this, sign_in_ForCustomer.class);
+            startActivity(intent);
+        });
 
-          if (Email.equals("")){
-              edtEmail.setError("This Place Cannot Be Empty");
-          }
+        btnSignUp.setOnClickListener(view -> {
 
-          if (Phone.equals("")){
-              edtPhone.setError("This Place Cannot Be Empty");
-          }
+            if(validateInput()){
+                //Send data in database
+                mAuth.createUserWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+//                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(customerSignUp.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                  //  updateUI(null);
+                                }
+                            }
+                        });
+            }
+        });
+    }
 
-          if (Password.equals("")){
-              edtPassword.setError("This Place Cannot Be Empty");
-          }
+    boolean validateInput()
+    {
+        String FullName = edtFullName.getText().toString().trim();
+        String Email = edtEmail.getText().toString().trim();
+        String Phone = edtPhone.getText().toString().trim();
+        String Password = edtPassword.getText().toString();
+        String ConfirmPassword = edtConfirmPassword.getText().toString();
 
-          if (ConfirmPassword.equals(Password)){
-              edtConfirmPassword.setError("Password Matched Successfully");
+        if (FullName.equals("")) {
+            edtFullName.setError("Name Cannot Be Empty");
+            return false;
+        }
+        else if (!isValidEmail(Email)) {
+            edtEmail.setError("Email Cannot Be Empty or Invalid");
+            return false;
+        }
+        else if (Phone.equals("")) {
+            edtPhone.setError("Phone Cannot Be Empty");
+            return false;
+        }
+        else if (Password.equals("")) {
+            edtPassword.setError("Password Cannot Be Empty");
+            return false;
+        }
+        else if (!ConfirmPassword.equals(Password)) {
+            edtConfirmPassword.setError("Password DidNot Match");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 
-          }
-          else {
-              edtConfirmPassword.setError("The Password DidNot Match");
-          }
-      });
-
-}}
+    public static boolean isValidEmail(CharSequence Email) {
+        return (!TextUtils.isEmpty(Email) && Patterns.EMAIL_ADDRESS.matcher(Email).matches());
+    }
+}
--- app/src/main/java/com/example/mayd/sign_in_ForCustomer.java
+++ app/src/main/java/com/example/mayd/sign_in_ForCustomer.java
@@ -1,49 +1,126 @@
 package com.example.mayd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class sign_in_ForCustomer extends AppCompatActivity {
-    EditText edtEmail,edtPassword;
+    private static final String TAG = "tag";
+    EditText edtEmail, edtPassword;
     Button btnSignIn;
     TextView tvSignUp;
     TextView tvForgotPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_for_customer);
 
-        edtEmail=findViewById(R.id.Customer_edtEmailSignIn);
-        edtPassword=findViewById(R.id.Customer_edtPasswordSignIn);
-        btnSignIn=findViewById(R.id.Customer_btnSignIn_P1);
+        edtEmail = findViewById(R.id.Customer_edtEmailSignIn);
+        edtPassword = findViewById(R.id.Customer_edtPasswordSignIn);
+        btnSignIn = findViewById(R.id.Customer_btnSignIn_P1);
 
-        tvSignUp=findViewById(R.id.Customer_tvSignUp_SignIn_P1);
-        tvForgotPassword=findViewById(R.id.Customer_tvForgotPassword_SignIn);
+        mAuth = FirebaseAuth.getInstance();
+
+        tvSignUp = findViewById(R.id.Customer_tvSignUp_SignIn_P1);
+        tvForgotPassword = findViewById(R.id.Customer_tvForgotPassword_SignIn);
 
 
         tvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(sign_in_ForCustomer.this, customerSignUp.class);
+                Intent intent = new Intent(sign_in_ForCustomer.this, customerSignUp.class);
                 startActivity(intent);
 
             }
         });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                if (validateInput()) {
+
+
+                    mAuth.signInWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                            .addOnCompleteListener(sign_in_ForCustomer.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Toast.makeText(sign_in_ForCustomer.this, "Sign In Successful", Toast.LENGTH_SHORT).show();
+                                        //updateUI(user);
+                                        Intent intent = new Intent(sign_in_ForCustomer.this, home1.class);
+                                        startActivity(intent);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(sign_in_ForCustomer.this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+
+                                        // updateUI(null);
+                                    }
+                                }
+                            });
+                }
+
+
+            }
+        });
+
 
         tvForgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(sign_in_ForCustomer.this, Forget_password_customer.class);
+                Intent intent = new Intent(sign_in_ForCustomer.this, Forget_password_customer.class);
                 startActivity(intent);
 
             }
         });
 
-    }}
+    }
+
+    boolean validateInput() {
+        String Email = edtEmail.getText().toString().trim();
+        String Password = edtPassword.getText().toString();
+
+        if (!isValidEmail(Email)) {
+            edtEmail.setError("Email Cannot Be Empty or Invalid");
+            return false;
+        } else if (Password.equals("")) {
+            edtPassword.setError("Password Cannot Be Empty");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static boolean isValidEmail(CharSequence Email) {
+        return (!TextUtils.isEmpty(Email) && Patterns.EMAIL_ADDRESS.matcher(Email).matches());
+    }
+
+}
+
