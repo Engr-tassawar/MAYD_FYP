f001dd73f2cb5ddb45d1ebae118fdfbaaa0212b9
HP <tassawarabbas034@gmail.com>
2022-07-17 01:34:17 -0700

    select_time_and_date

--- app/src/main/java/com/example/mayd/booking_schedule.java
+++ app/src/main/java/com/example/mayd/booking_schedule.java
@@ -3,10 +3,13 @@ package com.example.mayd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
@@ -14,18 +17,23 @@ import com.google.android.material.textfield.TextInputLayout;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
 
 public class booking_schedule extends AppCompatActivity {
-    TextInputEditText edtSelectDate;
+    TextInputEditText edtSelectDate,edtSelectTime;
+    TimePickerDialog timePickerDialog;
     int year;
     int day;
     int month;
+    int Hour;
+    int Minute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_schedule);
         edtSelectDate=findViewById(R.id.edtSelectDate);
-
+        edtSelectTime=findViewById(R.id.edtSelectTime);
         Calendar calender = Calendar.getInstance();
         edtSelectDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +50,25 @@ public class booking_schedule extends AppCompatActivity {
                     }
                 },year,month,day);
                 datePickerDialog.show();
+            }
+        });
+        edtSelectTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog.OnTimeSetListener onTimeSetListener= new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectHour, int selectMinute) {
+                        Hour = selectHour;
+                        Minute=selectMinute;
+                        edtSelectTime.setText(String.format(Locale.getDefault(),"%02d:%2d",Hour,Minute));
+
+                    }
+
+                };
+                TimePickerDialog timePickerDialog = new TimePickerDialog(booking_schedule.this,onTimeSetListener,Hour,Minute,true);
+                timePickerDialog.setTitle("Select Time");
+                timePickerDialog.show();
             }
         });
 
